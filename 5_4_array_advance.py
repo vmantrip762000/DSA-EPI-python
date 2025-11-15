8465fbaef58341df4a1671519ef23a6f5d723e1b
Mantri Aditya <159584214+vmantrip762000@users.noreply.github.com>
2025-11-15 02:07:38 -0500

    Add files via upload

---
+++ 5_4_array_advance.py
@@ -0,0 +1,13 @@
+def can_reach_end(A):
+    furthest_reached_so_far, max_end = 0, len(A)-1
+    i=0
+    while i <= furthest_reached_so_far and furthest_reached_so_far <= max_end:
+        
+        furthest_reached_so_far = max(i + A[i], furthest_reached_so_far)
+        print(furthest_reached_so_far)
+        i += 1
+    return furthest_reached_so_far >=  max_end
+
+print(can_reach_end([3,3,7,0,2,0,1]))
+print(can_reach_end([3,2,0,0,2,0,7]))
+print(can_reach_end([2,4,1,1,0,2,3]))
